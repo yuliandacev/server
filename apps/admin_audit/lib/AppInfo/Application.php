294a6291a9d2d3d9fa257aca21f0bd09536a1ca3
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-05-07 22:04:29 +0200

    fixup! Add dedicated API for apps' bootstrapping process
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- apps/admin_audit/lib/AppInfo/Application.php
+++ apps/admin_audit/lib/AppInfo/Application.php
@@ -49,6 +49,9 @@ use OCA\AdminAudit\Actions\UserManagement;
 use OCA\AdminAudit\Actions\Versions;
 use OCP\App\ManagerEvent;
 use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\Authentication\TwoFactorAuth\IProvider;
 use OCP\Console\ConsoleEvent;
 use OCP\IGroupManager;
@@ -59,205 +62,20 @@ use OCP\Share;
 use OCP\Util;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
-class Application extends App {
+class Application extends App implements IBootstrap {
 
 	/** @var ILogger */
 	protected $logger;
 
 	public function __construct() {
 		parent::__construct('admin_audit');
-		$this->initLogger();
 	}
 
-	public function initLogger() {
-		$c = $this->getContainer()->getServer();
-		$config = $c->getConfig();
-
-		$default = $config->getSystemValue('datadirectory', \OC::$SERVERROOT . '/data') . '/audit.log';
-		$logFile = $config->getAppValue('admin_audit', 'logfile', $default);
-		if ($logFile === null) {
-			$this->logger = $c->getLogger();
-			return;
-		}
-		$this->logger = $c->getLogFactory()->getCustomLogger($logFile);
+	public function register(IRegistrationContext $context): void {
+		$context->registerEventListener('a', 'b');
 	}
 
-	public function register() {
-		$this->registerHooks();
-	}
-
-	/**
-	 * Register hooks in order to log them
-	 */
-	protected function registerHooks() {
-		$this->userManagementHooks();
-		$this->groupHooks();
-		$this->authHooks();
-
-		$this->consoleHooks();
-		$this->appHooks();
-
-		$this->sharingHooks();
-
-		$this->fileHooks();
-		$this->trashbinHooks();
-		$this->versionsHooks();
-
-		$this->securityHooks();
-	}
-
-	protected function userManagementHooks() {
-		$userActions = new UserManagement($this->logger);
-
-		Util::connectHook('OC_User', 'post_createUser',	$userActions, 'create');
-		Util::connectHook('OC_User', 'post_deleteUser',	$userActions, 'delete');
-		Util::connectHook('OC_User', 'changeUser',	$userActions, 'change');
-
-		/** @var IUserSession|Session $userSession */
-		$userSession = $this->getContainer()->getServer()->getUserSession();
-		$userSession->listen('\OC\User', 'postSetPassword', [$userActions, 'setPassword']);
-		$userSession->listen('\OC\User', 'assignedUserId', [$userActions, 'assign']);
-		$userSession->listen('\OC\User', 'postUnassignedUserId', [$userActions, 'unassign']);
-	}
-
-	protected function groupHooks() {
-		$groupActions = new GroupManagement($this->logger);
-
-		/** @var IGroupManager|Manager $groupManager */
-		$groupManager = $this->getContainer()->getServer()->getGroupManager();
-		$groupManager->listen('\OC\Group', 'postRemoveUser',  [$groupActions, 'removeUser']);
-		$groupManager->listen('\OC\Group', 'postAddUser',  [$groupActions, 'addUser']);
-		$groupManager->listen('\OC\Group', 'postDelete',  [$groupActions, 'deleteGroup']);
-		$groupManager->listen('\OC\Group', 'postCreate',  [$groupActions, 'createGroup']);
-	}
-
-	protected function sharingHooks() {
-		$shareActions = new Sharing($this->logger);
-
-		Util::connectHook(Share::class, 'post_shared', $shareActions, 'shared');
-		Util::connectHook(Share::class, 'post_unshare', $shareActions, 'unshare');
-		Util::connectHook(Share::class, 'post_unshareFromSelf', $shareActions, 'unshare');
-		Util::connectHook(Share::class, 'post_update_permissions', $shareActions, 'updatePermissions');
-		Util::connectHook(Share::class, 'post_update_password', $shareActions, 'updatePassword');
-		Util::connectHook(Share::class, 'post_set_expiration_date', $shareActions, 'updateExpirationDate');
-		Util::connectHook(Share::class, 'share_link_access', $shareActions, 'shareAccessed');
-	}
-
-	protected function authHooks() {
-		$authActions = new Auth($this->logger);
-
-		Util::connectHook('OC_User', 'pre_login', $authActions, 'loginAttempt');
-		Util::connectHook('OC_User', 'post_login', $authActions, 'loginSuccessful');
-		Util::connectHook('OC_User', 'logout', $authActions, 'logout');
-	}
-
-	protected function appHooks() {
-		$eventDispatcher = $this->getContainer()->getServer()->getEventDispatcher();
-		$eventDispatcher->addListener(ManagerEvent::EVENT_APP_ENABLE, function (ManagerEvent $event) {
-			$appActions = new AppManagement($this->logger);
-			$appActions->enableApp($event->getAppID());
-		});
-		$eventDispatcher->addListener(ManagerEvent::EVENT_APP_ENABLE_FOR_GROUPS, function (ManagerEvent $event) {
-			$appActions = new AppManagement($this->logger);
-			$appActions->enableAppForGroups($event->getAppID(), $event->getGroups());
-		});
-		$eventDispatcher->addListener(ManagerEvent::EVENT_APP_DISABLE, function (ManagerEvent $event) {
-			$appActions = new AppManagement($this->logger);
-			$appActions->disableApp($event->getAppID());
-		});
-	}
-
-	protected function consoleHooks() {
-		$eventDispatcher = $this->getContainer()->getServer()->getEventDispatcher();
-		$eventDispatcher->addListener(ConsoleEvent::EVENT_RUN, function (ConsoleEvent $event) {
-			$appActions = new Console($this->logger);
-			$appActions->runCommand($event->getArguments());
-		});
-	}
-
-	protected function fileHooks() {
-		$fileActions = new Files($this->logger);
-		$eventDispatcher = $this->getContainer()->getServer()->getEventDispatcher();
-		$eventDispatcher->addListener(
-			IPreview::EVENT,
-			function (GenericEvent $event) use ($fileActions) {
-				/** @var File $file */
-				$file = $event->getSubject();
-				$fileActions->preview([
-					'path' => mb_substr($file->getInternalPath(), 5),
-					'width' => $event->getArguments()['width'],
-					'height' => $event->getArguments()['height'],
-					'crop' => $event->getArguments()['crop'],
-					'mode'  => $event->getArguments()['mode']
-				]);
-			}
-		);
-
-		Util::connectHook(
-			Filesystem::CLASSNAME,
-			Filesystem::signal_post_rename,
-			$fileActions,
-			'rename'
-		);
-		Util::connectHook(
-			Filesystem::CLASSNAME,
-			Filesystem::signal_post_create,
-			$fileActions,
-			'create'
-		);
-		Util::connectHook(
-			Filesystem::CLASSNAME,
-			Filesystem::signal_post_copy,
-			$fileActions,
-			'copy'
-		);
-		Util::connectHook(
-			Filesystem::CLASSNAME,
-			Filesystem::signal_post_write,
-			$fileActions,
-			'write'
-		);
-		Util::connectHook(
-			Filesystem::CLASSNAME,
-			Filesystem::signal_post_update,
-			$fileActions,
-			'update'
-		);
-		Util::connectHook(
-			Filesystem::CLASSNAME,
-			Filesystem::signal_read,
-			$fileActions,
-			'read'
-		);
-		Util::connectHook(
-			Filesystem::CLASSNAME,
-			Filesystem::signal_delete,
-			$fileActions,
-			'delete'
-		);
-	}
-
-	protected function versionsHooks() {
-		$versionsActions = new Versions($this->logger);
-		Util::connectHook('\OCP\Versions', 'rollback', $versionsActions, 'rollback');
-		Util::connectHook('\OCP\Versions', 'delete',$versionsActions, 'delete');
-	}
-
-	protected function trashbinHooks() {
-		$trashActions = new Trashbin($this->logger);
-		Util::connectHook('\OCP\Trashbin', 'preDelete', $trashActions, 'delete');
-		Util::connectHook('\OCA\Files_Trashbin\Trashbin', 'post_restore', $trashActions, 'restore');
-	}
-
-	protected function securityHooks() {
-		$eventDispatcher = $this->getContainer()->getServer()->getEventDispatcher();
-		$eventDispatcher->addListener(IProvider::EVENT_SUCCESS, function (GenericEvent $event) {
-			$security = new Security($this->logger);
-			$security->twofactorSuccess($event->getSubject(), $event->getArguments());
-		});
-		$eventDispatcher->addListener(IProvider::EVENT_FAILED, function (GenericEvent $event) {
-			$security = new Security($this->logger);
-			$security->twofactorFailed($event->getSubject(), $event->getArguments());
-		});
+	public function boot(IBootContext $context): void {
+		// TODO: Implement boot() method.
 	}
 }
